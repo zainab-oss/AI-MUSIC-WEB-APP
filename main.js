be897ac1fc5053f8bd87b3b8093b13c1190638ed
Serious Zainab <85008588+zainab-oss@users.noreply.github.com>
2023-04-19 11:36:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,12 +5,14 @@ leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 scoreLeftWrist = 0;
+scoreRightWrist = 0;
 status_song1 = "";
+status_song2 = "";
 
 function preload()
 {
-	song1 = loadSound("Mahzoon.mp3");
-    song2 = loadSound("the-way-of-the-tears.mp3");
+	song1 = loadSound("Agar Tum.mp3");
+    song2 = loadSound("Shukran Allah.mp3");
 
 }
 
@@ -32,19 +34,21 @@ function modelLoaded()
 
 function gotPoses(results)
 {
-    if(results.length > 0) {
-        console.log(results);
+  if(results.length > 0)
+  {
+	scoreLeftWrist =  results[0].pose.keypoints[9].score;
+	scoreRightWrist = results[0].pose.keypoints[10].score;
+	console.log("scoreRightWrist = " + scoreRightWrist + " scoreLeftWrist = " + scoreLeftWrist);
+	
+	rightWristX = results[0].pose.rightWrist.x;
+	rightWristY = results[0].pose.rightWrist.y;
+	console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
 
-        scoreLeftWrist = results[0].pose.keypoints[9].score;
-
-        leftWristX = results[0].pose.leftWrist.x;
-        leftWristY = results[0].pose.leftWrist.y;
-        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
-
-        rightWristX = results[0].pose.rightWrist.x;
-        rightWristY = results[0].pose.rightWrist.y;
-        console.log("rightWristX = " + rightWristX + 'rightWristY = ' + rightWristY);
-    }
+	leftWristX = results[0].pose.leftWrist.x;
+	leftWristY = results[0].pose.leftWrist.y;
+	console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+		
+  }
 }
 
 function draw()
@@ -59,17 +63,28 @@ function draw()
     if (scoreLeftWrist > 0.2)
     {
         circle(leftWristX, leftWristY, 20);
-        song_song2.stop();
-    }
+        song2.stop();
 
-    if(status_song1 == false)
-    {
-        song1.play();
-    }
-        else {
-
-            document.getElementById("song_name").innerHTML = "Mahzoon";
+        if(status_song1 == false)
+        {
+            song1.play();
+            document.getElementById("song_name").innerHTML = "Agar Tum";
         }
+    }
+
+    status_song2 = song2.isPlaying();
+
+    if (scoreRightWrist > 0.2)
+    {
+        circle(rightWristX, rightWristY, 20);
+        song1.stop();
+
+        if(status_song2 == false)
+    {
+        song2.play();
+        document.getElementById("song_name").innerHTML = "Shukran Allah";
+    }
+    }
        
 
     
